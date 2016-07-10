729371424198577eb2273fe48c14615af19d4997
kauvya <kauvya29@gmail.com>
2016-07-10 12:12:36 +0530

    Update towerofhanoi.c

--- towerofhanoi.c
+++ towerofhanoi.c
@@ -1,20 +1,68 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
-void towerOfHanoi(int n, char fromrod, char torod, char auxrod)
-{
-    if (n == 1)
-    {
-        printf("\n Move disk 1 from rod %c to rod %c", fromrod, torod);
-        return;
-    }
-    towerOfHanoi(n-1, fromrod, auxrod, torod);
-    printf("\n Move disk %d from rod %c to rod %c", n, fromrod, torod);
-    towerOfHanoi(n-1, auxrod, torod, fromrod);
-}
-
 int main()
 {
-    int n = 4; // Number of disks
-    towerOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods
-    return 0;
-}
+	vector <char> stack;
+	int n = 3;
+	int rings = 3;
+	char from = 'a';
+	char to = 'b';
+	char with = 'c';
+	char temp = ' ';
+	
+	int ret = 1;
+	int w = 0;    //moves
+	int moves = (pow (2, n) - 1);  //expected moves(2^n) - 1
+	
+        while(w <= moves)              
+	{
+		if(ret == 1)
+		{
+	       	   do
+         	   {  
+		    stack.push_back(to);
+                    stack.push_back(from);
+		     temp = to;
+		     to = with;
+		     with = temp;
+		     from = from;
+		    
+		     ret = 2;
+		     n--;
+		   }while(n >= 1);
+	 	   
+		 }
+
+		 if( ret == 2)
+		  {
+		     
+	  	        cout << "move ring from tower " << stack.back();
+		        stack.pop_back();
+		        cout << " to tower " << stack.back() << endl;
+		       stack.pop_back();
+		        w++;
+			if(w >= moves)
+			{
+			  break;
+			}
+		        ret = 3;
+		       
+		   }   
+		  
+		  if(ret == 3)
+		   {
+		     cout << "move ring from tower " << stack.back();
+		     stack.pop_back();
+		     cout << " to tower " << stack.back() << endl;
+		     stack.pop_back();
+		     w++;
+		     temp = with;
+		     with = from;
+		     from = temp;
+		    
+		     to = to;
+		     n = rings;
+		     ret = 1;
+		    }
+		}
+	}
